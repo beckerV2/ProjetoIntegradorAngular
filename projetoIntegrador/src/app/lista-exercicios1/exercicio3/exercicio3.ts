b2582f3eb4ab9798368b3fe3899ee4e7b97c29a9
beckerV2 <beckerfelipe60@gmail.com>
2026-09-15 00:45:50 +0000

    ex3

---
+++ projetoIntegrador/src/app/lista-exercicios1/exercicio3/exercicio3.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercicio3',
+  standalone: false,
+  templateUrl: './exercicio3.html',
+  styleUrl: './exercicio3.scss',
+})
+export class Exercicio3 {
+  imagemUrl = 'https://agristar.com.br/upload/blog/original/planejamento-no-cultivo-de-abobora-cabotia-impulsiona-negocios-de-agricultores-familiares--08-03-2024-16-00-06-308.jpg';
+}
