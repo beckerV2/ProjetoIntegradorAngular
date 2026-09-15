42de978593bd8ca890101f09e76ddf48937a6fd9
beckerV2 <beckerfelipe60@gmail.com>
2026-09-15 00:55:16 +0000

    ex4

--- projetoIntegrador/src/app/lista-exercicios1/lista-exercicios1-module.ts
+++ projetoIntegrador/src/app/lista-exercicios1/lista-exercicios1-module.ts
@@ -6,9 +6,10 @@ import { ListaExercicios1RoutingModule } from './lista-exercicios1-routing-modul
 import { Exercicio1 } from './exercicio1/exercicio1';
 import { Exercicio2 } from './exercicio2/exercicio2';
 import { Exercicio3 } from './exercicio3/exercicio3';
+import { Exercicio4 } from './exercicio4/exercicio4';
 
 @NgModule({
-  declarations: [Exercicio1, Exercicio2, Exercicio3],
+  declarations: [Exercicio1, Exercicio2, Exercicio3, Exercicio4],
   imports: [CommonModule, ListaExercicios1RoutingModule, FormsModule],
 })
 export class ListaExercicios1Module {}
