c4a228a519deb2c6abc28bad7b0091291f43c91f
beckerV2 <beckerfelipe60@gmail.com>
2026-09-15 00:25:12 +0000

    ex2

--- projetoIntegrador/src/app/lista-exercicios1/lista-exercicios1-routing-module.ts
+++ projetoIntegrador/src/app/lista-exercicios1/lista-exercicios1-routing-module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { Exercicio1 } from './exercicio1/exercicio1';
+import { Exercicio2 } from './exercicio2/exercicio2';
 
 const routes: Routes = [
   {path: 'exercicio1', component: Exercicio1},
+  {path: 'exercicio2', component: Exercicio2}
 ];
 
 @NgModule({
