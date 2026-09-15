4d89173bf220976ef9034ef02b5b91fc19c5de76
beckerV2 <beckerfelipe60@gmail.com>
2026-09-15 01:17:02 +0000

    ex6

---
+++ projetoIntegrador/src/app/lista-exercicios1/exercicio6/exercicio6.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercicio6',
+  standalone: false,
+  templateUrl: './exercicio6.html',
+  styleUrl: './exercicio6.scss',
+})
+export class Exercicio6 {
+  quantidade = 0;
+
+  subtrair() {
+    if (this.quantidade > 0){
+    this.quantidade--;
+    }
+  }
+  adicionar(){
+    this.quantidade++
+  }
+}
